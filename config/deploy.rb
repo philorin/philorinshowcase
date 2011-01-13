f473b0dcfb25c8644d002703ef94a89e7219c61f
Phil Orin <phil.orin@gmail.com>
2011-01-12 22:03:46 -0800

    all new table layout

---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+set :application, "philorinshowcase"
+set :repository,  "git@GitServer:philorin/philorinshowcase.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "www.philorin.com"                          # Your HTTP server, Apache/etc
+role :app, "www.philorin.com"                          # This may be the same as your `Web` server
+role :db,  "www.philorin.com", :primary => true # This is where Rails migrations will run
+# role :db,  "www.philorin.com"
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
